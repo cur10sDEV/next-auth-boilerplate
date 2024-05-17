21a4aeb14e3e94aa738d06ac2ee5911eccdcafd5
cur10sDEV <specsx@protonmail.com>
2024-05-17 13:08:30 +0530

    complete next-auth setup

---
+++ src/nextAuth/auth.config.ts
@@ -0,0 +1,5 @@
+import type { NextAuthConfig } from "next-auth";
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+
+export default { providers: [Github, Google] } satisfies NextAuthConfig;
