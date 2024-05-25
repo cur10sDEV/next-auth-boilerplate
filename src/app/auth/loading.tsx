f04e6b62c63e32c32ead2d786c43d55781a626d6
cur10sDEV <specsx@protonmail.com>
2024-05-25 15:55:38 +0530

    deploy changes

---
+++ src/app/auth/loading.tsx
@@ -0,0 +1,9 @@
+const AuthLoading = () => {
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center items-center bg-white bg-opacity-20 z-50 gap-4">
+      <span className="text-6xl">🧐</span>
+      <p className="text-2xl">Please give us a moment...</p>
+    </div>
+  );
+};
+export default AuthLoading;
